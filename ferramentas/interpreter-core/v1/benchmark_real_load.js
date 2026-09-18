c6182361a0d83b81ce0fce3722eca3df82991c58
vinialbuquerquepitstop-beep <vinialbuquerque.pitstop@gmail.com>
2026-09-17 22:36:19 -0300

    test(interpreter): expose multi-field mismatch aggregate

--- ferramentas/interpreter-core/v1/benchmark_real_load.js
+++ ferramentas/interpreter-core/v1/benchmark_real_load.js
@@ -460,6 +460,7 @@ const diagnostic = {
   condition_pair_provenance: divergence.condition_pair_provenance,
   condition_scope_diagnostics: divergence.condition_scope_diagnostics,
   wrong_price_diagnostics: divergence.wrong_price_diagnostics,
+  multi_mismatch_diagnostics: divergence.multi_mismatch_diagnostics,
   ambiguities_by_cause: divergence.ambiguities_by_cause,
   ambiguities_by_field: divergence.ambiguities_by_field,
   top_model_gaps: divergence.top_model_gaps
