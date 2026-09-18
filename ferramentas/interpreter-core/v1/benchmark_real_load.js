36c271edb7db779ed161942eec1702749faac547
vinialbuquerquepitstop-beep <vinialbuquerque.pitstop@gmail.com>
2026-09-17 21:43:56 -0300

    test(interpreter): print CPO scope diagnostics

--- ferramentas/interpreter-core/v1/benchmark_real_load.js
+++ ferramentas/interpreter-core/v1/benchmark_real_load.js
@@ -212,6 +212,7 @@ const diagnostic = {
   mismatch_signatures: divergence.mismatch_signatures,
   condition_pairs: divergence.condition_pairs,
   condition_pair_provenance: divergence.condition_pair_provenance,
+  condition_scope_diagnostics: divergence.condition_scope_diagnostics,
   ambiguities_by_cause: divergence.ambiguities_by_cause,
   ambiguities_by_field: divergence.ambiguities_by_field,
   top_model_gaps: divergence.top_model_gaps
